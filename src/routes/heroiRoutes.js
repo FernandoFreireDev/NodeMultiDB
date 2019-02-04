b5ee893853a741f92da83cbf588e46d1c3ff9792
Fernando Freire <fernando.ricardo.freire@gmail.com>
2019-02-04 14:11:53 -0200

    0.8

--- src/routes/heroiRoutes.js
+++ src/routes/heroiRoutes.js
@@ -1,7 +1,7 @@
 const BaseRoutes = require('./base/baseRoutes');
 
 class HeroiRoutes extends BaseRoutes {
-    constructor(db){
+    constructor(db) {
         super();
         this.db = db;
     }
@@ -11,20 +11,35 @@ class HeroiRoutes extends BaseRoutes {
             path: '/herois',
             method: 'GET',
             handler: (request, headers) => {
-                return this.db.read();
+                try {
+                    const {
+                        skip,
+                        limit,
+                        nome
+                    } = request.query;
+                    let query = {};
+                    if(nome){
+                        query.nome = nome;
+                    }
+
+                    if(isNaN(skip) && skip){
+                        throw Error('O tipo do skip é incorreto');
+                    }
+
+                    if(isNaN(limit) && limit){
+                        throw Error('O tipo do limit é incorreto');
+                    }                    
+
+                    return this.db.read(query, parseInt(skip), parseInt(limit));
+                }
+                catch (error) {
+                    console.log('Erro: ', error);
+                    return 'Erro interno no servidor';
+                }
             }
         }
     }
 
-    listTodos() {
-        return {
-            path: '/listar',
-            method: 'GET',
-            handler: (request, headers) => {
-                return this.db.read();
-            }
-        }
-    }
 }
 
 module.exports = HeroiRoutes;
--- src/tests/apiHerois.test.js
+++ src/tests/apiHerois.test.js
@@ -3,7 +3,7 @@ const api = require('./../api');
 
 let app = {};
 
-describe('Suite de testes da API Herois', function(){
+describe.only('Suite de testes da API Herois', function(){
 
     this.beforeAll(async () => {
         app = await api;
@@ -13,7 +13,7 @@ describe('Suite de testes da API Herois', function(){
 
         const result = await app.inject({
             method: 'GET',
-            url: '/herois'
+            url: '/herois?skip=0&limit=10'
         });
 
         const dados = JSON.parse(result.payload);
@@ -22,6 +22,47 @@ describe('Suite de testes da API Herois', function(){
         assert.deepEqual(statusCode, 200);
         assert.ok(Array.isArray(dados));
 
+    });
+
+    it('Listar /herois - deve retornar somente 10 registros', async () => {
+        const TAMANHO_LIMITE = 3;
+
+        const result = await app.inject({
+            method: 'GET',
+            url: `/herois?skip=0&limit=${TAMANHO_LIMITE}`
+        });
+
+        const dados = JSON.parse(result.payload);
+        const statusCode = result.statusCode;
+
+        assert.deepEqual(statusCode, 200);
+        assert.ok(dados.length === TAMANHO_LIMITE);
+    });
+
+    it('Listar /herois - deve retornar um erro com limit incorreto', async () => {
+        const TAMANHO_LIMITE = 'AEEE';
+
+        const result = await app.inject({
+            method: 'GET',
+            url: `/herois?skip=0&limit=${TAMANHO_LIMITE}`
+        });
+
+        assert.deepEqual(result.payload, 'Erro interno no servidor');
+    });
+
+    it('Listar /herois - deve filtrar um item', async () => {
+        const TAMANHO_LIMITE = 1000;
+        const NAME = "Homem-Aranha-Mon Feb 04 2019 13:19:09 GMT-0200 (Horário brasileiro de verão)";
+        const result = await app.inject({
+            method: 'GET',
+            url: `/herois?skip=0&limit=${TAMANHO_LIMITE}&nome=${NAME}`
+        });
+
+        const dados = JSON.parse(result.payload);
+        const statusCode = result.statusCode;
+
+        assert.deepEqual(statusCode, 200);
+        assert.ok(dados[0].nome === NAME);
     });
 
 });
