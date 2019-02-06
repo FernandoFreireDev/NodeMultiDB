88a18e6b256fe18997c8fa622cdbeca25dbce0de
Fernando Freire <fernando.ricardo.freire@gmail.com>
2019-02-05 22:48:41 -0200

    1.0

--- src/routes/heroiRoutes.js
+++ src/routes/heroiRoutes.js
@@ -1,5 +1,8 @@
 const BaseRoutes = require('./base/baseRoutes');
 const Joi = require('joi');
+const failAction = (request, headers, error) => {
+    throw error;
+}
 
 class HeroiRoutes extends BaseRoutes {
     constructor(db) {
@@ -13,9 +16,7 @@ class HeroiRoutes extends BaseRoutes {
             method: 'GET',
             config: {
                 validate: {
-                    failAction: (request, headers, erro) => {
-                        throw erro;
-                    },
+                    failAction,
                     query: {
                         skip: Joi.number().integer().default(0),
                         limit: Joi.number().integer().default(10),
@@ -42,6 +43,74 @@ class HeroiRoutes extends BaseRoutes {
                 catch (error) {
                     console.log('Erro: ', error);
                     return 'Erro interno no servidor';
+                }
+            }
+        }
+    }
+
+    create() {
+        return {
+            path: '/herois',
+            method: 'POST',
+            config: {
+                validate: {
+                    failAction,
+                    payload: {
+                        nome: Joi.string().required().min(3).max(100),
+                        poder: Joi.string().required().min(2).max(100)
+                    }
+                }
+            },
+            handler: async (request) => {
+                try {
+                    const { nome, poder } = request.payload;
+                    const result = await this.db.create({ nome, poder });
+                    return {
+                        message: "Heroi cadastrado com sucesso",
+                        _id: result._id
+                    }
+                } catch (error) {
+                    console.error('Deu erro: ', error);
+                    return 'Erro Interno';
+                }
+            }
+        }
+    }
+
+    update() {
+        return {
+            path: '/herois/{id}',
+            method: 'PATCH',
+            config: {
+                validate: {
+                    params: {
+                        id: Joi.string().required()
+                    },
+                    payload: {
+                        nome: Joi.string().min(3).max(100),
+                        poder: Joi.string().min(2).max(100)
+                    }
+                },
+            },
+            handler: async (request) => {
+                try {
+                    const { id } = request.params;
+                    const { payload } = request;
+
+                    const dadosString = JSON.stringify(payload);
+                    const dados = JSON.parse(dadosString);
+
+                    const result = await this.db.update(id, dados);
+                    if(result.nModified !== 1) return {
+                        message: 'Não foi possível atualizar'
+                    }
+                    
+                    return {
+                        message: 'Heroi atualizado com sucesso'
+                    }
+                } catch (error) {
+                    console.error('Deu Erro: ', error);
+                    return 'Erro interno';
                 }
             }
         }
--- src/tests/apiHerois.test.js
+++ src/tests/apiHerois.test.js
@@ -3,10 +3,30 @@ const api = require('./../api');
 
 let app = {};
 
+const DEFAULT_CADASTRAR = {
+    nome: 'Chapolin Colorado',
+    poder: 'Marreta Biônica'
+}
+
+const DEFAULT_ATUALIZAR = {
+    nome: 'Gavião Negro',
+    poder: 'Flechas'
+}
+
+let DEFAULT_ID = '';
+
 describe('Suite de testes da API Herois', function () {
 
     this.beforeAll(async () => {
         app = await api;
+        const result = await app.inject({
+            method: 'POST',
+            url: '/herois',
+            payload: JSON.stringify(DEFAULT_ATUALIZAR)
+        });
+
+        const dados = JSON.parse(result.payload);
+        DEFAULT_ID = dados._id;
     });
 
     it('Listar Herois', async () => {
@@ -63,7 +83,7 @@ describe('Suite de testes da API Herois', function () {
 
     it('Listar /herois - deve filtrar um item', async () => {
         const TAMANHO_LIMITE = 1000;
-        const NAME = "Homem-Aranha-Mon Feb 04 2019 13:19:09 GMT-0200 (Horário brasileiro de verão)";
+        const NAME = "Batman";
         const result = await app.inject({
             method: 'GET',
             url: `/herois?skip=0&limit=${TAMANHO_LIMITE}&nome=${NAME}`
@@ -74,6 +94,62 @@ describe('Suite de testes da API Herois', function () {
 
         assert.deepEqual(statusCode, 200);
         assert.ok(dados[0].nome === NAME);
+    });
+
+    it('Cadastrar POST - /herois', async () => {
+        const result = await app.inject({
+            method: 'POST',
+            url: `/herois`,
+            payload: JSON.stringify(DEFAULT_CADASTRAR)
+        });
+
+        const statusCode = result.statusCode;
+        const {
+            message,
+            _id
+        } = JSON.parse(result.payload);
+
+        assert.ok(statusCode === 200);
+        assert.notStrictEqual(_id, undefined);
+        assert.deepEqual(message, "Heroi cadastrado com sucesso");
+    });
+
+    it('Atualizar PATCH - /herois/:id', async () => {
+        const _id = DEFAULT_ID;
+        const expected = {
+            poder: 'Arco e Flecha'
+        }
+
+        const result = await app.inject({
+            method: 'PATCH',
+            url: `/herois/${_id}`,
+            payload: JSON.stringify(expected)
+        });
+
+        const statusCode = result.statusCode;
+        const dados = JSON.parse(result.payload);
+
+        assert.ok(statusCode === 200);
+        assert.deepEqual(dados.message, 'Heroi atualizado com sucesso');
+    });
+
+    it('Atualizar PATCH - /herois/:id - Não deve atualizar com id incorreto', async () => {
+        const _id = `5c5a2172b68c0216ec79e5f6`;
+        const expected = {
+            poder: 'Arco e Flecha'
+        }
+
+        const result = await app.inject({
+            method: 'PATCH',
+            url: `/herois/${_id}`,
+            payload: JSON.stringify(expected)
+        });
+
+        const statusCode = result.statusCode;
+        const dados = JSON.parse(result.payload);
+
+        assert.ok(statusCode === 200);
+        assert.deepEqual(dados.message, 'Não foi possível atualizar');
     });
 
 });
